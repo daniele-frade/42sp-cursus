f20fa61d297094f9fb770f0270fb576733f33e94
Daniele Frade <dfrade@student.42sp.org.br>
2023-11-18 16:53:35 -0300

    Add Get_Next_Line and Printf functions to Libft

---
+++ 01_libft/src/printf/printf.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfrade <dfrade@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 14:03:47 by dfrade            #+#    #+#             */
+/*   Updated: 2023/11/18 14:09:54 by dfrade           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *string, ...)
+{
+	va_list	args;
+	size_t	i;
+	int		length;
+
+	if (string == NULL)
+		return (-1);
+	va_start(args, string);
+	i = 0;
+	length = 0;
+	while (string[i] != '\0')
+	{
+		if (string[i] == '%')
+		{
+			i++;
+			length += ft_format_handler(args, string[i]);
+		}
+		else
+			length += ft_putchar(string[i]);
+		i++;
+	}
+	va_end(args);
+	return (length);
+}
+
+int	ft_format_handler(va_list args, char format)
+{
+	int	length;
+
+	length = 0;
+	if (format == 'c')
+		length += ft_putchar((char)va_arg(args, int));
+	else if (format == 's')
+		length += ft_putstr(va_arg(args, char *));
+	else if (format == 'p')
+		length += ft_putptr(va_arg(args, unsigned long), HEX_LW);
+	else if (format == 'd' || format == 'i')
+		length += ft_putnbr_base(va_arg(args, int), DEC);
+	else if (format == 'u')
+		length += ft_putnbr_base(va_arg(args, unsigned int), DEC);
+	else if (format == 'x')
+		length += ft_putnbr_base(va_arg(args, unsigned int), HEX_LW);
+	else if (format == 'X')
+		length += ft_putnbr_base(va_arg(args, unsigned int), HEX_UP);
+	else if (format == '%')
+		length += ft_putchar('%');
+	return (length);
+}
---
+++ 01_libft/src/printf/printf_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfrade <dfrade@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 14:04:26 by dfrade            #+#    #+#             */
+/*   Updated: 2023/11/18 14:09:46 by dfrade           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_strlen(char *str)
+{
+	int	length;
+
+	length = 0;
+	while (str[length])
+		length++;
+	return (length);
+}
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(char *str)
+{
+	int	length;
+
+	if (str == NULL)
+		return (ft_putstr("(null)"));
+	length = 0;
+	while (str[length] != '\0')
+	{
+		write(1, &str[length], 1);
+		length++;
+	}
+	return (length);
+}
+
+int	ft_putptr(unsigned long nb, char *base)
+{
+	int	length;
+	int	base_length;
+
+	length = 0;
+	base_length = ft_strlen(base);
+	if (nb == 0)
+		return (ft_putstr("(nil)"));
+	if (nb >= (unsigned long)base_length)
+		length += ft_putptr((nb / base_length), base);
+	else
+		length += ft_putstr("0x");
+	length += ft_putchar(base[nb % base_length]);
+	return (length);
+}
+
+int	ft_putnbr_base(long int nb, char *base)
+{
+	int	length;
+	int	base_length;
+
+	length = 0;
+	base_length = ft_strlen(base);
+	if (nb < 0)
+	{
+		length += ft_putchar('-');
+		nb = nb * -1;
+	}
+	if (nb >= base_length)
+		length += ft_putnbr_base((nb / base_length), base);
+	length += ft_putchar(base[nb % base_length]);
+	return (length);
+}
